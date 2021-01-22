8cb54f0a7f7f99223c180016727424d679d6b80c
q4speed <kun@fit2cloud.com>
2021-01-22 13:55:23 +0800

    style: 调整scss

--- demo/main.js
+++ demo/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import ElementUI from 'element-ui';
-import '@/scss/index.scss';
+import '@/styles/index.scss';
 import router from "./router";
 
 Vue.config.productionTip = false
