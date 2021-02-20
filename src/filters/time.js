e324343888bf886f87849c7e54c9f4a38750881b
q4speed <kun@fit2cloud.com>
2021-02-20 18:11:09 +0800

    fix(控件): 修复时间过滤器bug

--- src/filters/time.js
+++ src/filters/time.js
@@ -1,5 +1,13 @@
-export const datetimeFormat = timestamp => {
+const getCheckDate = timestamp => {
+  if (!timestamp) return false
   const dt = new Date(timestamp)
+  if (isNaN(dt.getTime())) return false
+  return dt
+}
+export const datetimeFormat = timestamp => {
+  const dt = getCheckDate(timestamp);
+  if (!dt) return timestamp
+
   const y = dt.getFullYear()
   const m = (dt.getMonth() + 1 + '').padStart(2, '0')
   const d = (dt.getDate() + '').padStart(2, '0')
@@ -11,7 +19,9 @@ export const datetimeFormat = timestamp => {
 }
 
 export const dateFormat = timestamp => {
-  const dt = new Date(timestamp)
+  const dt = getCheckDate(timestamp);
+  if (!dt) return timestamp
+
   const y = dt.getFullYear()
   const m = (dt.getMonth() + 1 + '').padStart(2, '0')
   const d = (dt.getDate() + '').padStart(2, '0')
