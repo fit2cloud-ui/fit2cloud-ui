08a7002f7c9e295a23dd92883b28189443eb36cb
mokun <kun@fit2cloud.com>
2021-02-22 21:15:04 +0800

    refactor(控件): 优化搜索条

--- src/components/search-bar/FuSearchContions.vue
+++ src/components/search-bar/FuSearchContions.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="fu-search-conditions">
     <div class="fu-search-conditions__item" v-for="(condition, index) in conditions" :key="index">
-      <div>{{ condition.label }}</div>
-      <div>{{ condition.operatorLabel }}</div>
+      <div v-if="condition.label">{{ condition.label }}</div>
+      <div v-if="condition.operatorLabel">{{ condition.operatorLabel }}</div>
       <div class="condition-value">{{ condition.valueLabel }}</div>
       <i class="el-icon-close condition-remove" @click="remove(index)"/>
     </div>
