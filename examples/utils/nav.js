19fa00b52fb2561e51027d7e9c60ecd113423217
王丹 <wangdan@wangdandeMBP.lan>
2021-02-26 10:52:46 +0800

    wd

--- examples/utils/nav.js
+++ examples/utils/nav.js
@@ -2,11 +2,11 @@ const navList =  {
     "组件":[
         {
             name:'ComplexTable 复合表格',
-            path:'complex-table'
+            path:'table'
         },
         {
             name:'SearchBar 搜索条',
-            path:'search-bar'
+            path:'search'
         },
         
     ],
