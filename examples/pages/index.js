86a81756c5f91cad3cb7e5aced9976b929e4f40d
王丹 <wangdan@wangdandeMBP.lan>
2021-02-26 14:53:09 +0800

    wd

---
+++ examples/pages/index.js
@@ -0,0 +1,8 @@
+/* Components */
+import Vue from 'vue'
+import SearchDemo from './search/SearchDemo'
+import SpeedDialDemo from './speed-dial/SpeedDialDemo'
+import ComplexTableDemo from './table/TableDemo'
+Vue.component(SearchDemo.name, SearchDemo)
+Vue.component(SpeedDialDemo.name, SpeedDialDemo)
+Vue.component(ComplexTableDemo.name, ComplexTableDemo)
