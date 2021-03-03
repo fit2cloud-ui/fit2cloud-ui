8169314c1e8c32970fdd10ab31de5afce283e3fd
wangdan <dan.wang@fit2cloud.com>
2021-03-03 14:15:55 +0800

    table

---
+++ examples/pages/table/index.vue
@@ -0,0 +1,32 @@
+<example path="./examples/ComplexTableDemo.vue"/>
+
+<template>
+  <div>
+    <document-table :data="attributes" />
+    <code-example
+      title="ComplexTable"
+      :component="examples['complex-table-demo']"
+    />
+  </div>
+</template>
+
+<script>
+import examples from "../../mixins/docsExample";
+import attributes from "./attributes";
+export default {
+  name: "TableDoc",
+  mixins: [examples],
+  data() {
+    return {
+      attributes,
+      value1: '',
+    };
+  },
+  methods: {},
+  created() {},
+  mounted() {},
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
