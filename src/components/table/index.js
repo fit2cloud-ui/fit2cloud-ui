b03fa0af01c4b55a88a1e401187ce55360d082b4
q4speed <kun@fit2cloud.com>
2021-03-09 16:53:37 +0800

    refactor(控件): 组件入口加载所有子组件

--- src/components/table/index.js
+++ src/components/table/index.js
@@ -1,9 +1,13 @@
 import FuTable from "./FuTable";
 import FuTablePagination from "./FuTablePagination";
+import FuTableOperations from "./table-operations";
+import FuTableColumnSelect from "./table-column-select";
 
 FuTable.install = function (Vue) {
   Vue.component(FuTable.name, FuTable);
   Vue.component(FuTablePagination.name, FuTablePagination);
+  Vue.use(FuTableOperations)
+  Vue.use(FuTableColumnSelect)
 };
 
 export default FuTable;
