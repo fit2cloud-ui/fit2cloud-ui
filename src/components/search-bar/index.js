429301355df1ed4cfa4767d4ba7b2a5ba387b7d5
q4speed <kun@fit2cloud.com>
2021-03-10 11:40:01 +0800

    refactor(控件): 提供SearchBarButton

--- src/components/search-bar/index.js
+++ src/components/search-bar/index.js
@@ -1,9 +1,11 @@
 import FuSearchBar from "./FuSearchBar";
 import FuQuickSearch from "./FuQuickSearch";
+import FuSearchBarButton from "./FuSearchBarButton";
 import FuComplexComponents from "./complex-components";
 
 FuSearchBar.install = function (Vue) {
   Vue.component(FuSearchBar.name, FuSearchBar);
+  Vue.component(FuSearchBarButton.name, FuSearchBarButton);
   Vue.component(FuQuickSearch.name, FuQuickSearch);
   Vue.use(FuComplexComponents);
 };
