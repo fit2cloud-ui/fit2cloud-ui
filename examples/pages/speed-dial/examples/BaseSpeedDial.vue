2f3595c8cda969264428cd51ffc678e8a874c219
q4speed <kun@fit2cloud.com>
2021-03-14 10:02:37 +0800

    docs(文档): 修改SpeedDial文档

---
+++ examples/pages/speed-dial/examples/BaseSpeedDial.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="speed-dial-demo">
+    <div>
+      <fu-speed-dial id="left" size="big" direction="left" :items="items" @click="click"/>
+    </div>
+
+    <div>
+      <fu-speed-dial id="top" :items="items" @click="click" type="success"
+                     icon="el-icon-video-play" active-icon="el-icon-video-pause"/>
+    </div>
+
+    <div>
+      <fu-speed-dial id="right" size="medium" direction="right" type="warning" :items="items" @click="click"/>
+    </div>
+
+    <div>
+      <fu-speed-dial id="bottom" size="small" direction="bottom" type="danger" :items="items" @click="click"/>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BaseSpeedDial",
+  data() {
+    return {
+      items: [
+        {title: "Item 1", click: this.clickItem},
+        {title: "Item 2", click: this.clickItem},
+        {title: "Item 3", click: this.clickItem},
+        {title: "Item 4", click: this.clickItem},
+        {title: "Item 5", click: this.clickItem},
+      ]
+    }
+  },
+  methods: {
+    click(obj, e) {
+      console.log(obj, e)
+    },
+    clickItem(item, e) {
+      console.log(item, e)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.speed-dial-demo {
+  display: flex;
+  align-items: center;
+
+  > div {
+    width: 25%;
+  }
+}
+</style>
