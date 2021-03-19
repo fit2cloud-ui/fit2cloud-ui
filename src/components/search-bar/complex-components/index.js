dfe5ef3b84f903eec785ed9ed06396e02b82e955
q4speed <kun@fit2cloud.com>
2021-03-19 15:59:04 +0800

    fix(控件): 修复搜索组件样式不生效的bug

--- src/components/search-bar/complex-components/index.js
+++ src/components/search-bar/complex-components/index.js
@@ -1,7 +1,11 @@
+import locale from "@/locale";
+
 const components = require.context('./', true, /\.vue$/)
 
 const FuComplexComponents = {
-  install: function (Vue) {
+  install: function (Vue, opts = {}) {
+    locale.use(opts.locale);
+    locale.i18n(opts.i18n);
     components.keys().map(key => {
       const component = components(key).default
       Vue.component(component.name, component);
