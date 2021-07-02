a19ee97a4fff67660ec1c2ec1853b9bbcdb0a438
q4speed <kun@fit2cloud.com>
2021-07-02 15:45:16 +0800

    feat(国际化): 增加繁体中文zh-TW

---
+++ src/locale/lang/zh-TW.js
@@ -0,0 +1,44 @@
+export default {
+  fu: {
+    search_bar: {
+      search: "搜索",
+      adv_search: "高級搜索",
+      ok: "確定",
+      cancel: "取消",
+      please_select: "請選擇",
+      please_input: "請輸入",
+      like: "包含",
+      not_like: "不包含",
+      in: "屬於",
+      not_in: "不屬於",
+      gt: "大於",
+      ge: "大於等於",
+      lt: "小於",
+      le: "小於等於",
+      eq: "等於",
+      ne: "不等於",
+      between: "之間",
+      select_date: '選擇日期',
+      start_date: '開始日期',
+      end_date: '結束日期',
+      select_date_time: '選擇日期時間',
+      start_date_time: '開始日期時間',
+      end_date_time: '結束日期時間',
+      range_separator: "至",
+      data_time_error: "開始日期不能大於結束日期",
+      clean: "清空",
+      refresh: "刷新",
+    },
+    table: {
+      ok: "確定",
+      custom_table_fields: "自定義表格字段",
+      custom_table_fields_desc: "固定字段不在選擇範圍",
+    },
+    steps: {
+      cancel: "取消",
+      next: "下一步",
+      prev: "上一步",
+      finish: "完成",
+    }
+  }
+}
